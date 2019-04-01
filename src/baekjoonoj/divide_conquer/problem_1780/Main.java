ee5b71a1f156edcc3e33388669d6f61db4690799
Jeong Seok Hyeon <arcane1028@gmail.com>
2019-04-01 16:24:57 +0900

    Refactoring boj 1780

--- src/baekjoonoj/divide_conquer/problem_1780/Main.java
+++ src/baekjoonoj/divide_conquer/problem_1780/Main.java
@@ -4,10 +4,6 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int count_one = 0;
-    static int count_zero = 0;
-    static int count_minus = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -21,46 +17,39 @@ public class Main {
                 a[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        int count[] = new int[3];
 
-        int t = check(a, 0, 0, N);
-        if (t == 1) {
-            count_one += 1;
-        } else if (t == 0) {
-            count_zero += 1;
-        } else if (t==-1){
-            count_minus += 1;
+        check(count, a, 0, 0, N);
+
+        for (int i = 0; i < 3; i++) {
+            bw.write(count[i]+"\n");
         }
-        bw.write(count_minus + "\n");
-        bw.write(count_zero + "\n");
-        bw.write(count_one + "\n");
-
         br.close();
         bw.close();
     }
 
-    private static int check(int a[][], int x, int y, int size) {
-        if (size == 1){
-            return a[x][y];
-        }
+    private static boolean isSame(int a[][], int x, int y, int size) {
         for (int i = x; i < x + size; i++) {
             for (int j = y; j < y + size; j++) {
                 if (a[x][y] != a[i][j]) {
-                    for (int k = x; k < x + size; k += size / 3) {
-                        for (int l = y; l < y + size; l += size / 3) {
-                            int t = check(a, k, l, size / 3);
-                            if (t == 1) {
-                                count_one += 1;
-                            } else if (t == 0) {
-                                count_zero += 1;
-                            } else if (t==-1){
-                                count_minus += 1;
-                            }
-                        }
-                    }
-                    return -100;
+                    return false;
                 }
             }
         }
-        return a[x][y];
+        return true;
+    }
+
+    private static void check(int count[], int a[][], int x, int y, int size) {
+        if (isSame(a, x, y, size)) {
+            count[a[x][y] + 1] += 1;
+            return;
+        }
+
+        for (int i = x; i < x + size; i += size / 3) {
+            for (int j = y; j < y + size; j += size / 3) {
+                check(count, a, i, j, size / 3);
+            }
+        }
+        return;
     }
 }
