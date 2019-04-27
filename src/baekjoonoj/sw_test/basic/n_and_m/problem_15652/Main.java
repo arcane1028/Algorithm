e132d76b6440cdcbc2ddd7c8315a794db0172856
Jeong Seok Hyeon <arcane1028@gmail.com>
2019-04-27 12:40:19 +0900

    Solved boj 15652

---
+++ src/baekjoonoj/sw_test/basic/n_and_m/problem_15652/Main.java
@@ -0,0 +1,39 @@
+package baekjoonoj.sw_test.basic.n_and_m.problem_15652;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int n, m;
+    private static int seq[];
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        seq = new int[m];
+        sb = new StringBuilder();
+
+        solution(1, 0);
+        bw.write(sb.toString());
+        br.close();
+        bw.close();
+    }
+
+    private static void solution(int start, int len) {
+        if (len == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(seq[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            seq[len] = i;
+            solution(i,len + 1);
+        }
+    }
+}
