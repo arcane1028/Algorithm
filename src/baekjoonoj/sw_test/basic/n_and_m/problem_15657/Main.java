86f0adbf951d0a74ad21d103905c9b6224f1a622
Jeong Seok Hyeon <arcane1028@gmail.com>
2019-05-01 19:18:55 +0900

    Solved boj 15657

---
+++ src/baekjoonoj/sw_test/basic/n_and_m/problem_15657/Main.java
@@ -0,0 +1,49 @@
+package baekjoonoj.sw_test.basic.n_and_m.problem_15657;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int n, m;
+    private static int seq[];
+    private static int num[];
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        seq = new int[m];
+        num = new int[n];
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+        solution(0, 0);
+
+        bw.write(sb.toString());
+        br.close();
+        bw.close();
+    }
+
+    private static void solution(int start, int len) {
+        if (len == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(seq[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            seq[len] = num[i];
+            solution(i, len + 1);
+        }
+    }
+}
