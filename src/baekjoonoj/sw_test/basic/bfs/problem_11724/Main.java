9f94786c6224b191b96ab77976582c3ec18d6644
Jeong Seok Hyeon <arcane1028@gmail.com>
2019-05-12 14:52:45 +0900

    Add dfs solution boj 11724

--- src/baekjoonoj/sw_test/basic/bfs/problem_11724/Main.java
+++ src/baekjoonoj/sw_test/basic/bfs/problem_11724/Main.java
@@ -2,6 +2,66 @@ package baekjoonoj.sw_test.basic.bfs.problem_11724;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static ArrayList<Integer> v[];
+    private static boolean check[];
+    private static int answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        v = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            v[i] = new ArrayList<>();
+        }
+
+        for (int i = 0, v1, v2; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            v1 = Integer.parseInt(st.nextToken());
+            v2 = Integer.parseInt(st.nextToken());
+            v[v1].add(v2);
+            v[v2].add(v1);
+        }
+        answer = 0;
+        check = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            if (check[i]) {
+                continue;
+            }
+            check[i] = true;
+            answer++;
+            dfs(i);
+        }
+
+
+        bw.write(answer + "\n");
+
+        br.close();
+        bw.close();
+    }
+
+    private static void dfs(int p) {
+
+        for (int i = 0; i < v[p].size(); i++) {
+            int tmp = v[p].get(i);
+            if (check[tmp]) {
+                continue;
+            }
+            check[tmp] =true;
+            dfs(tmp);
+        }
+    }
+}
+/*
+import java.io.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -57,3 +117,4 @@ public class Main {
         bw.close();
     }
 }
+*/
