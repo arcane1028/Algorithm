c078f06dbc163aa05d0175d6c4cfd19124f5f824
unknown <arcane1028@gmail.com>
2019-06-18 12:46:42 +0900

    Optimize code boj 2225

--- src/baekjoonoj/sw_test/basic/dp/problem_2225/Main.java
+++ src/baekjoonoj/sw_test/basic/dp/problem_2225/Main.java
@@ -11,31 +11,19 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        if (k == 1) {
-            bw.write("1\n");
-        } else {
-            long[][] dp = new long[n + 1][k];
-            long sum = 0;
-            for (int i = 0; i <= n; i++) {
-                dp[i][1] = 1;
-            }
-            for (int i = 0; i < k; i++) {
-                dp[0][i] = 1;
-            }
+        int[][] dp = new int[k + 1][n + 1];
 
-            for (int i = 2; i < k; i++) {
-                for (int j = 1; j <= n; j++) {
-                    dp[j][i] = (dp[j][i - 1] + dp[j - 1][i]) % 1_000_000_000;
-                }
-            }
-
-            for (int i = 0; i <= n; i++) {
-                sum = (sum + dp[i][k - 1]) % 1_000_000_000;
-            }
-
-            bw.write(sum + "\n");
+        for (int i = 0; i <= k; i++) {
+            dp[i][0] = 1;
         }
 
+        for (int i = 1; i <= k; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % 1_000_000_000;
+            }
+        }
+
+        bw.write(dp[k][n] + "\n");
         br.close();
         bw.close();
     }
